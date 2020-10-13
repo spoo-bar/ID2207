9757e912e85a0593ae36d0048d99a4a33ce23b9f
Spoorthi S <spoon611@users.noreply.github.com>
2020-10-13 20:00:30 +0200

    adding event tests

---
+++ SEP/Tests/EventTest.cs
@@ -0,0 +1,101 @@
+﻿using BusinessTier;
+using DataTier;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class EventTest
+    {
+        EventController eventController = new EventController();
+
+        [TestMethod]
+        public void CreateTest()
+        {
+            // Arrange
+            var eventRequest = new EventRequest("123", new Client("Peter", "Pen", "238740291"), "Party",
+                                                DateTime.Now, DateTime.Now.AddDays(1),
+                                                33, 321);
+
+            // Act
+            var ev = eventController.Create(eventRequest);
+
+            // Assert
+            Assert.AreEqual(eventRequest.RecordNr, ev.RecordNr);
+            Assert.AreEqual(eventRequest.Client.GetHashCode(), ev.Client.GetHashCode());
+            Assert.AreEqual(eventRequest.EventType, ev.EventType);
+            Assert.AreEqual(eventRequest.From, ev.From);
+            Assert.AreEqual(eventRequest.To, ev.To);
+            Assert.AreEqual(eventRequest.Attendees, ev.Attendees);
+            Assert.AreEqual(eventRequest.Budget, ev.Budget);
+        }
+
+        [TestMethod]
+        public void CreateTaskTest()
+        {
+            // Arrange
+            var eventRequest = new EventRequest("123", new Client("Peter", "Pen", "238740291"), "Party",
+                                                DateTime.Now, DateTime.Now.AddDays(1),
+                                                33, 321);
+            eventController.Create(eventRequest);
+            var eventTask = new EventTask()
+            {
+                Description = "test description",
+                AssignedTo = new User() { Email = "abcd@xy.z" }
+            };
+
+            // Act
+            var ev = eventController.CreateTask("123", eventTask);
+
+            // Assert
+            Assert.IsTrue(ev.Tasks.Count == 1);
+            Assert.IsNotNull(ev.Tasks.First().TaskID);
+            Assert.AreEqual(eventTask.Description, ev.Tasks.First().Description);
+            Assert.AreEqual(eventTask.AssignedTo.Email, ev.Tasks.First().AssignedTo.Email);
+        }
+
+        [TestMethod]
+        public void CreateTaskEventDoesNotExistTest()
+        {
+            // Arrange
+            var eventTask = new EventTask()
+            {
+                Description = "test description",
+                AssignedTo = new User() { Email = "abcd@xy.z" }
+            };
+
+            // Act
+            var exception = Assert.ThrowsException<ApplicationException>(() => eventController.CreateTask("123", eventTask));
+
+
+            // Assert
+            Assert.AreEqual("Could not find task with that record number", exception.Message);
+        }
+
+        [TestMethod]
+        public void GetAllEvents()
+        {
+            // Arrange
+            var eventRequest = new EventRequest("123", new Client("Peter", "Pen", "238740291"), "Party",
+                                                DateTime.Now, DateTime.Now.AddDays(1),
+                                                33, 321);
+            eventController.Create(eventRequest);
+            var eventTask = new EventTask()
+            {
+                Description = "test description",
+                AssignedTo = new User() { Email = "abcd@xy.z" }
+            };
+
+            // Act
+            var events = eventController.GetEvents();
+
+            // Assert
+            Assert.IsTrue(events.Count >= 2);
+        }
+    }
+}
